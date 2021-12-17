dc4e8f826f641cca566d0025c7fbf8af91ffb190
thomgro <thomas.grosser@hotmail.de>
2021-12-17 12:34:54 +0000

    day 8 star 2 started

--- day08/day08.py
+++ day08/day08.py
@@ -18,3 +18,27 @@ for output in outputs:
             count += 1
 
 print("star 1: %i" % count)
+
+assert(len(inputs) == len(outputs))
+for i in range(len(inputs)):
+    digitStrings = inputs[i].split()
+    #print(digits)
+    twoSegs = tuple([x for x in digitStrings if len(x) == 2])
+    threeSegs = tuple([x for x in digitStrings if len(x) == 3])
+    fourSegs = tuple([x for x in digitStrings if len(x) == 4])
+    fiveSegs = tuple([x for x in digitStrings if len(x) == 5])
+    sixSegs = tuple([x for x in digitStrings if len(x) == 6])
+    sevenSegs = tuple([x for x in digitStrings if len(x) == 7])
+    #print(sevenSegs)
+    one = twoSegs[0]
+    four = fourSegs[0]
+    seven = threeSegs[0]
+    eight = sevenSegs[0]
+    digitMap = {
+        one: '1',
+        four: '4',
+        seven: '7',
+        eight: '8',
+    }
+    #print(digitMap)
+    
