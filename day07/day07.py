9b4c34d059799d222c8cb23b4098913c902cd61e
thomgro <thomas.grosser@hotmail.de>
2021-12-17 09:23:10 +0000

    day 7 star 2

--- day07/day07.py
+++ day07/day07.py
@@ -7,24 +7,30 @@ def calcDistance(array, target):
         distance += abs(num - target)
     return distance
 
+def calcComplexDistance(array, target):
+    distance = 0
+    for num in array:
+        steps = abs(num-target)
+        if steps > 0:
+            for i in range(steps, 0, -1):
+                distance += i
+    return distance
+
+
 crabs = openFileAsLineArray("input")[0].split(",")
 crabs = [int(x) for x in crabs]
 
 crabs.sort()
-min = crabs[0]
 max = crabs[-1]
 
-shortestDistance = 999999999
-position = -1
 distances = []
 for i in range(max+1):
-    #distance = calcDistance(crabs, i)
-    #if distance < shortestDistance:
-    #    shortestDistance = distance
-    #    position = i
-    #print(i)
-    #print(distance)
     distances.append(calcDistance(crabs, i))
-
 distances.sort()
 print("star 1: %i" % distances[0])
+
+compDistances = []
+for i in range(max+1):
+    compDistances.append(calcComplexDistance(crabs, i))
+compDistances.sort()
+print("star 2: %i" % compDistances[0])
